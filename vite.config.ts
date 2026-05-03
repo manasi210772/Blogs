615426eef8399a8fe09b001d647bdcbc2a1ede55
Manasi Patil <manasi210772@gmail.com>
2026-05-03 08:32:58 +0530

    ready for hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
 export default defineConfig({
-  base: '/Blogs/', 
+  plugins: [
+    react(),
+    mdx({
+      remarkPlugins: [
+        remarkFrontmatter,
+        remarkMdxFrontmatter,
+      ],
+    }),
+  ],
 })
