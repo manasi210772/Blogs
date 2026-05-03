c25d0df8af6f2f06c43b28156eb23057a571fa7e
Manasi Patil <manasi210772@gmail.com>
2026-05-03 08:10:53 +0530

    edits for hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,5 @@
-import { defineConfig } from "vite";
-import path from "path";
-import mdx from "@mdx-js/rollup";
-import react from "@vitejs/plugin-react";
-import rehypePrism from "rehype-prism-plus";
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    mdx({
-      rehypePlugins: [rehypePrism],
-    }),
-  ],
-  base: "/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  base: '/Blogs/', 
+})
