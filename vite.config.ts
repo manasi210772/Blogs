e7f5e0b74922ea2685e69815bbf4a085720a82f3
Manasi Patil <manasi210772@gmail.com>
2026-06-18 23:21:25 +0530

    Removing bugs before deploying

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
 export default defineConfig({
-  base: '/',
+  base: '/food-blog/',
 
   plugins: [
     react(),
